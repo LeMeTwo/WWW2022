c7e370482bebedd47ca43bc6b99d237463f46a2b
Krzychu-Z <zerebecki.k@gmail.com>
2022-07-29 23:07:20 +0200

    Fix the bug of queuing hover event over room divs

--- index.js
+++ index.js
@@ -2,42 +2,42 @@ $().ready(function() {
     $(".active-counter h4").text($(".count").length);
     $("#room-1").hover(
         function () {
-            $("#room-1 .rooms-users").slideDown().removeClass("default-room");
+            $("#room-1 .rooms-users").stop().slideDown().removeClass("default-room");
         },
         function () {
-            $("#room-1 .rooms-users").slideUp().addClass("default-room");
+            $("#room-1 .rooms-users").stop().slideUp().addClass("default-room");
         }
-    )
+    );
     $("#room-2").hover(
         function () {
-            $("#room-2 .rooms-users").slideDown().removeClass("default-room");
+            $("#room-2 .rooms-users").stop().slideDown().removeClass("default-room");
         },
         function () {
-            $("#room-2 .rooms-users").slideUp().addClass("default-room");
+            $("#room-2 .rooms-users").stop().slideUp().addClass("default-room").off("mouseenter mouseleave");
         }
     )
     $("#room-3").hover(
         function () {
-            $("#room-3 .rooms-users").slideDown().removeClass("default-room");
+            $("#room-3 .rooms-users").stop().slideDown().removeClass("default-room");
         },
         function () {
-            $("#room-3 .rooms-users").slideUp().addClass("default-room");
+            $("#room-3 .rooms-users").stop().slideUp().addClass("default-room").off("mouseenter mouseleave");
         }
     )
     $("#room-4").hover(
         function () {
-            $("#room-4 .rooms-users").slideDown().removeClass("default-room");
+            $("#room-4 .rooms-users").stop().slideDown().removeClass("default-room");
         },
         function () {
-            $("#room-4 .rooms-users").slideUp().addClass("default-room");
+            $("#room-4 .rooms-users").stop().slideUp().addClass("default-room").off("mouseenter mouseleave");
         }
     )
     $("#room-5").hover(
         function () {
-            $("#room-5 .rooms-users").slideDown().removeClass("default-room");
+            $("#room-5 .rooms-users").stop().slideDown().removeClass("default-room");
         },
         function () {
-            $("#room-5 .rooms-users").slideUp().addClass("default-room");
+            $("#room-5 .rooms-users").stop().slideUp().addClass("default-room").off("mouseenter mouseleave");
         }
     )
 
