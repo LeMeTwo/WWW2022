e2eabc18e44706948f23247ec1908c2dd379dd4a
Krzychu-Z <zerebecki.k@gmail.com>
2022-08-15 18:33:13 +0200

    Fix major bugs
    This includes:
    - center moving text in rooms description
    - remove excess space under rooms

--- index.js
+++ index.js
@@ -2,42 +2,62 @@ $().ready(function() {
     $(".active-counter h4").text($(".count").length);
     $("#room-1").hover(
         function () {
-            $("#room-1 .rooms-users").stop().slideDown().removeClass("default-room");
+            $("#room-1 .rooms-users").stop().slideDown().removeClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(2)").removeClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(1)").addClass("hide-me");
         },
         function () {
-            $("#room-1 .rooms-users").stop().slideUp().addClass("default-room");
+            $("#room-1 .rooms-users").stop().slideUp().addClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(2)").addClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(1)").removeClass("hide-me");
         }
     );
     $("#room-2").hover(
         function () {
-            $("#room-2 .rooms-users").stop().slideDown().removeClass("default-room");
+            $("#room-2 .rooms-users").stop().slideDown().removeClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(2)").removeClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(1)").addClass("hide-me");
         },
         function () {
-            $("#room-2 .rooms-users").stop().slideUp().addClass("default-room").off("mouseenter mouseleave");
+            $("#room-2 .rooms-users").stop().slideUp().addClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(2)").addClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(1)").removeClass("hide-me");
         }
     )
     $("#room-3").hover(
         function () {
-            $("#room-3 .rooms-users").stop().slideDown().removeClass("default-room");
+            $("#room-3 .rooms-users").stop().slideDown().removeClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(2)").removeClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(1)").addClass("hide-me");
         },
         function () {
-            $("#room-3 .rooms-users").stop().slideUp().addClass("default-room").off("mouseenter mouseleave");
+            $("#room-3 .rooms-users").stop().slideUp().addClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(2)").addClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(1)").removeClass("hide-me");
         }
     )
     $("#room-4").hover(
         function () {
-            $("#room-4 .rooms-users").stop().slideDown().removeClass("default-room");
+            $("#room-4 .rooms-users").stop().slideDown().removeClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(2)").removeClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(1)").addClass("hide-me");
         },
         function () {
-            $("#room-4 .rooms-users").stop().slideUp().addClass("default-room").off("mouseenter mouseleave");
+            $("#room-4 .rooms-users").stop().slideUp().addClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(2)").addClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(1)").removeClass("hide-me");
         }
     )
     $("#room-5").hover(
         function () {
-            $("#room-5 .rooms-users").stop().slideDown().removeClass("default-room");
+            $("#room-5 .rooms-users").stop().slideDown().removeClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(2)").removeClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(1)").addClass("hide-me");
         },
         function () {
-            $("#room-5 .rooms-users").stop().slideUp().addClass("default-room").off("mouseenter mouseleave");
+            $("#room-5 .rooms-users").stop().slideUp().addClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(2)").addClass("hide-me");
+            $("#room-1 > div.rooms-title > p:nth-child(1)").removeClass("hide-me");
         }
     )
 
