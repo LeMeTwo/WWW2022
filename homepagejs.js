a581a857be57330ca6c3f64c13637f5a8d3ab779
Krzychu-Z <zerebecki.k@gmail.com>
2022-09-02 00:38:11 +0200

    Daily update - main page finally finished

--- homepagejs.js
+++ homepagejs.js
@@ -1,23 +1,3 @@
 $().ready(function() {
 
-    /*
-    var waypoint = new Waypoint({
-       element: $('.count-me'),
-       handler: function(direction) {
-
-       }
-    });
-
-    waypoint;
-
-    $(document).on('scroll', function() {
-    var pixelsFromTop = $(document).scrollTop();
-    if (pixelsFromTop > 500) {
-    });
-
-     */
-    $('.count-me').counterUp({
-        delay: 10,
-        time: 2000
-    });
 });
