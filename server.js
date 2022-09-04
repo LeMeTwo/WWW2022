63da354008ea4e96c48594c8558332a56431d6b9
Jakub Kryca <72783966+LeMeTwo@users.noreply.github.com>
2022-09-04 14:14:59 +0200

    Początek API

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const io = require('socket.io')(3000)
 
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 const users = {}
 const {Client} = require('pg')
 const DATABASE_HOST='labagh.pl';
@@ -25,6 +31,63 @@ const main = async () => {
       //await clientA.end();
       console.log("Slychac dobrze")
   }
+
+
+//RestfulAPI
+//Get from DB
+var server = app.listen(8081, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
+})
+
+//Sprawdź, czy konto już instnieje, możn użyć też do logowania 
+app.post('/CanRegister', jsonParser, async function (req, res) {
+  const body = req.body;
+  console.log(req.body)
+  //No validation, I`m too lazy bruh
+
+
+
+  //Pewnie do poprawki, na pewno da się zrobić lepiej
+  const clientA = new Client({
+    user: DATABASE_USER,
+    password: DATABASE_PASSWORD,
+    database: DATABASE_NAME,
+    host: DATABASE_HOST,
+    }); 
+
+    await clientA.connect();
+    var result = (await clientA.query('SELECT login FROM usersdb WHERE id=0', function(err, rows, fields) {
+
+      if (rows.rowCount == 0) {
+        res.end(JSON.stringify("Można stworzyć konto"))
+        return
+      }
+
+      else{res.end(JSON.stringify(rows.rows));}
+
+    }))
+    console.log("/CanRegister?")
+})
+
+
+//Tu będzie dodawanie do bazy danych
+//Jeszcze niegotowe
+app.get('/AddToDB',  async function (req, res) {
+  console.log(req)
+  //Pewnie do poprawki, na pewno da się zrobić lepiej
+  const clientA = new Client({
+    user: DATABASE_USER,
+    password: DATABASE_PASSWORD,
+    database: DATABASE_NAME,
+    host: DATABASE_HOST,
+    }); 
+
+    await clientA.connect();
+    await clientA.query('INSERT INTO table_name (id, login, email, password) VALUES (value1, value2, value3, value4);')
+    console.log("/AddToDB?")
+})
 };
 
 main().catch(console.error);
