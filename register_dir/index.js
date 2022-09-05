2c1354e005e24f349935a170dda8465187c054a2
Krzysztof Żerebecki <krzysztof.zerebecki@stazysta.comarch.pl>
2022-09-05 17:40:43 +0200

    Rest API pt I - design JSON form

--- register_dir/index.js
+++ register_dir/index.js
@@ -1,4 +1,16 @@
 $().ready(function (){
+
+    // Define POST method with fetch API
+    async function postData(data = {}, suffix) {        
+        // Default options are marked with *n
+        url = 'https://domain.com/' + suffix;
+        const response = await fetch(url, {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          body: JSON.stringify(data)
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+    }
+
     $("#passDisplay").click(showPass);
     function showPass() {
         var x = document.getElementById("pass");
@@ -8,4 +20,28 @@ $().ready(function (){
             x.type = "password";
         }
     }
+
+    // Submit listener
+    $(".form-alert").hide();
+
+    $(".my-form > form > div > button").click(function(e){
+        if ($("#ageConfirm").is(':checked') && $("#rulesConfirm").is(':checked')) {
+            var nick = $("#nick").val();
+            var password = $("#pass").val();
+            var email = $("#mail").val();
+            var generateJson = {};
+
+            // Regex for password validation
+            if (!(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[@$!%*?&-]).{8,}$/.test(password))) {
+                $(".form-alert").show().text("Your password does not meet our password criteria! You need to figure out something more difficult!");
+            } else {
+                generateJson.nick = nick;
+                generateJson.password = password;
+                generateJson.email = email;
+                postData(); //TODO
+            }
+        } else {
+            $(".form-alert").show().text("You need to confirm that you are 15 years old or above and have read and accept Use Terms!");
+        }
+    });
 });
