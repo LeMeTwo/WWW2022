45c38459488b118bf2214239582fa5cd03951dd8
Krzysztof Żerebecki <krzysztof.zerebecki@stazysta.comarch.pl>
2022-09-08 10:40:28 +0200

    Fast update

--- logged/index.js
+++ logged/index.js
@@ -1,5 +1,12 @@
 $().ready(function() {
-    //REST API POST
+    // REST API GET
+    async function getData(suffix) {
+        url = 'http://127.0.0.1:8081/' + suffix;                     
+        return await fetch(url).then(res => res.json());
+    }
+
+
+    // REST API POST
     async function postData(data = {}, suffix) {
         // Default options are marked with *n
         url = 'http://127.0.0.1:8081/' + suffix;
@@ -17,33 +24,37 @@ $().ready(function() {
     var myId = {};
     myId.id = localStorage['id'];
 
-    var roomData = postData(myId, 'api/roomInfo').then((response) => response.json())
-                    .then((responseJSON) => {
-                        if (responseJSON.Response === "WRONG") {
-                            $("#room-place").append("You don't belong to any room :(");
-                        } else {
-                            for (let i = 0; i < responseJSON.length; i++) {
-                                var base = '<a class="list-group-item" href="ChatRoom.html">\
-                                                <div class="rooms" id="room-' + (i + 1) + '">\
-                                                    <img class="rooms-backgr" src="rooms_db/room_1.png" alt="room#1">\
-                                                    <div class="rooms-users hide-me">' + responseJSON[i].memberCount + ' usr' + '</div>\
-                                                    <div class="rooms-title">\
-                                                        <p style="margin: 0">' + responseJSON[i].name + '</p>\
-                                                        <p style="margin: 0" class="hide-me">' + '\
-                                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + responseJSON[i].name + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
-                                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
-                                                            Aktywni teraz: ' + responseJSON[i].activeUsers + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
-                                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ostatnia aktywność:' + responseJSON[i].lastUpdate + '\
-                                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
-                                                        ' + '</p>\
-                                                    </div>\
-                                                </div>\
-                                            </a>';
-                                $("#room-place").append(base);
-                            }
-                        }
-                    });
+    postData(myId, 'api/roomInfo').then((response) => response.json())
+    .then((responseJSON) => {
+        if (responseJSON.Response === "WRONG") {
+            $("#room-place").append("You don't belong to any room :(");
+        } else {
+            for (let i = 0; i < responseJSON.length; i++) {
+                var base = '<a class="list-group-item" href="ChatRoom.html/' + responseJSON[i].roomID + '">\
+                                <div class="rooms" id="room-' + (i + 1) + '">\
+                                    <img class="rooms-backgr" src="rooms_db/room_1.png" alt="room#1">\
+                                    <div class="rooms-users hide-me">' + responseJSON[i].memberCount + ' usr' + '</div>\
+                                    <div class="rooms-title">\
+                                        <p style="margin: 0">' + responseJSON[i].name + '</p>\
+                                        <p style="margin: 0" class="hide-me">' + '\
+                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + responseJSON[i].name + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
+                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
+                                            Aktywni teraz: ' + responseJSON[i].activeUsers + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
+                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ostatnia aktywność:' + responseJSON[i].lastUpdate + '\
+                                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
+                                        ' + '</p>\
+                                    </div>\
+                                </div>\
+                            </a>';
+                $("#room-place").append(base);
+            }
+        }
+    });
 
+    getData('api/friendsInfo').then((response) => response.json())
+    .then((responseJSON) => {
+
+    });
 
     // Personalised user interface
     $("#user-name-logo").text(localStorage['login']);
