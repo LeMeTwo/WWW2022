f5d009622630cac19f60f97a5ab50344448aa490
Ablesh1 <72945140+Ablesh1@users.noreply.github.com>
2022-09-08 15:26:02 +0200

    Change path

--- login_dir/index.js
+++ login_dir/index.js
@@ -46,7 +46,7 @@ $().ready(function (){
                     } else {
                         localStorage['id'] = responseJSON.ID;
                         localStorage['login'] = login;
-                        window.location.replace("../logged/MainPage.html");
+                        window.location.replace("../MainPage.html");
                     }
                 });
         }
