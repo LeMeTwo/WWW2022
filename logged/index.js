101bb887ae6452bc179ba488f7642d0e7c40eb9f
Krzysztof Żerebecki <krzysztof.zerebecki@stazysta.comarch.pl>
2022-09-08 12:19:56 +0200

    Automatise rooms event listeners

--- logged/index.js
+++ logged/index.js
@@ -1,9 +1,9 @@
 $().ready(function() {
     // REST API GET
-    async function getData(suffix) {
-        url = 'http://127.0.0.1:8081/' + suffix;                     
-        return await fetch(url).then(res => res.json());
-    }
+    //async function getData(suffix) {
+    //    url = 'http://127.0.0.1:8081/' + suffix;                     
+    //    return await fetch(url).then(res => res.json());
+    //}
 
 
     // REST API POST
@@ -47,79 +47,33 @@ $().ready(function() {
                                 </div>\
                             </a>';
                 $("#room-place").append(base);
+                $("#room-" + (i + 1)).hover(
+                    function () {
+                        $("#room-" + (i + 1) + " .rooms-users").stop().slideDown().removeClass("hide-me");
+                        $("#room-" + (i + 1) + " > div.rooms-title > p:nth-child(2)").removeClass("hide-me");
+                        $("#room-" + (i + 1) + " > div.rooms-title > p:nth-child(1)").addClass("hide-me");
+                    },
+                    function () {
+                        $("#room-" + (i + 1) + " .rooms-users").stop().slideUp().addClass("hide-me");
+                        $("#room-" + (i + 1) + " > div.rooms-title > p:nth-child(2)").addClass("hide-me");
+                        $("#room-" + (i + 1) + " > div.rooms-title > p:nth-child(1)").removeClass("hide-me");
+                    }
+                );
             }
         }
     });
 
-    getData('api/friendsInfo').then((response) => response.json())
-    .then((responseJSON) => {
-
-    });
+    //getData('api/friendsInfo').then((response) => response.json())
+    //.then((responseJSON) => {
+    //
+    //});
 
     // Personalised user interface
     $("#user-name-logo").text(localStorage['login']);
 
+    // Count friends online
     $(".active-counter h4").text($(".count").length);
-    $("#room-1").hover(
-        function () {
-            $("#room-1 .rooms-users").stop().slideDown().removeClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(2)").removeClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(1)").addClass("hide-me");
-        },
-        function () {
-            $("#room-1 .rooms-users").stop().slideUp().addClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(2)").addClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(1)").removeClass("hide-me");
-        }
-    );
-    $("#room-2").hover(
-        function () {
-            $("#room-2 .rooms-users").stop().slideDown().removeClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(2)").removeClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(1)").addClass("hide-me");
-        },
-        function () {
-            $("#room-2 .rooms-users").stop().slideUp().addClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(2)").addClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(1)").removeClass("hide-me");
-        }
-    )
-    $("#room-3").hover(
-        function () {
-            $("#room-3 .rooms-users").stop().slideDown().removeClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(2)").removeClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(1)").addClass("hide-me");
-        },
-        function () {
-            $("#room-3 .rooms-users").stop().slideUp().addClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(2)").addClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(1)").removeClass("hide-me");
-        }
-    )
-    $("#room-4").hover(
-        function () {
-            $("#room-4 .rooms-users").stop().slideDown().removeClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(2)").removeClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(1)").addClass("hide-me");
-        },
-        function () {
-            $("#room-4 .rooms-users").stop().slideUp().addClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(2)").addClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(1)").removeClass("hide-me");
-        }
-    )
-    $("#room-5").hover(
-        function () {
-            $("#room-5 .rooms-users").stop().slideDown().removeClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(2)").removeClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(1)").addClass("hide-me");
-        },
-        function () {
-            $("#room-5 .rooms-users").stop().slideUp().addClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(2)").addClass("hide-me");
-            $("#room-1 > div.rooms-title > p:nth-child(1)").removeClass("hide-me");
-        }
-    )
+    
 
     $("#submitmsg").click(function () {
         var clientmsg = $("#usermsg").val();
